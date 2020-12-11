41327946ca80a9fca49f05dd8414c4ee3287a0a3
SMARTIUM <dev@smartium.io>
2020-12-11 19:23:10 -0300

    Set Static path to public folder; move index.html to public folder; Create style.css in public folder; Change theme to black; Set MongoDB URL to get from Heroku env variables.

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
 const compression = require('compression')
-const express = require('express');
+const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
 const app = express();
-const port = process.env.PORT || 3000
-
-const atlas = 'mongodb://dbuser:dev1357@cluster0-shard-00-00.ftckb.mongodb.net:27017,cluster0-shard-00-01.ftckb.mongodb.net:27017,cluster0-shard-00-02.ftckb.mongodb.net:27017/quotes-app?ssl=true&replicaSet=atlas-13518p-shard-0&authSource=admin&retryWrites=true&w=majority'
-const docker = 'mongodb://localhost:27117'
+const PORT = process.env.PORT || 3000
+var MONGO_URL = null
+if (app.get('env') == 'development') {
+	MONGO_URL = 'mongodb://localhost:27117'
+}
+else {
+	MONGO_URL = 'mongodb://dbuser:dev1357@cluster0-shard-00-00.ftckb.mongodb.net:27017,cluster0-shard-00-01.ftckb.mongodb.net:27017,cluster0-shard-00-02.ftckb.mongodb.net:27017/quotes-app?ssl=true&replicaSet=atlas-13518p-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
 
 
 // PROMISES
-MongoClient.connect(atlas, { useUnifiedTopology: true })
+MongoClient.connect(MONGO_URL, { useUnifiedTopology: true })
 	.then(client => {
 		console.log(`MongoDB connected @ ${client.s.url}`);
 		const db = client.db('quotes-app')
 		const Quotes = db.collection('quotes')
 
 		app.use(compression())
+		app.use(express.static(path.join(__dirname, 'public')))
 		app.use(bodyParser.urlencoded({ extended: true }))
 
-		app.listen(port, () => {
-			console.log(`Server started @ port ${port} in ${app.get('env').toUpperCase()} mode`)
+		app.listen(PORT, () => {
+			console.log(`Server started @ port ${PORT} in ${app.get('env').toUpperCase()} mode`)
 		})
 
 		app.get('/', (req, res) => {
-			res.sendFile(__dirname + '/index.html')
+			res.sendFile(__dirname + '/public/index.html')
 		})
 
 		app.post('/quotes', (req, res) => {
